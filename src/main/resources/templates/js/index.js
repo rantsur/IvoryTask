04270511c133d363163e4af75a9d80e1ba7c6e4e
rants <rantsur7@gmail.com>
2018-07-09 16:28:31 +0300

    disabled "edit" button whole modal is open

--- src/main/resources/templates/js/index.js
+++ src/main/resources/templates/js/index.js
@@ -1,6 +1,7 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
     var ctrl = this;
+    ctrl.editBtnFlag = false;
     ctrl.enterAllData = function () {
         $http.get("http://localhost:8080/allData")
             .then(function (response) {
@@ -17,12 +18,19 @@ app.controller('myCtrl', function ($scope, $http) {
     };
     ctrl.edit = function (customer) {
         ctrl.editCustomer = angular.copy(customer);
+        ctrl.editBtnFlag = true;
     };
+
+    ctrl.modalClosed = function () {
+        ctrl.editBtnFlag = false;
+    };
+
     ctrl.update = function () {
         $http.post("http://localhost:8080/edit", ctrl.editCustomer)
             .then(function (response) {
                 window.alert('Customer Details Updated Successfully');
                 $('#myModal').modal('hide');
+                ctrl.editBtnFlag = false;
                 location.reload();
             });
     };
